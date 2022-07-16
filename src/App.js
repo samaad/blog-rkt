b675dbc5ff5d2e34ec5b3324f34dea6407a49c0f
shoaib <shoaib.doulatabad@gmail.com>
2022-07-17 00:19:43 +0400

    basic syn with rkt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import PostList from "./features/post/PostList";
+import AddPostForm from "./features/post/AddPostForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <AddPostForm />
+        <PostList />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import postsReducer from "../features/post/postsSlice";
+import usersReducer from "../features/users/userSlices";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postsReducer,
+    users: usersReducer,
   },
 });
